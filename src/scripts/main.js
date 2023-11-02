0f0ed5282b6ce91993a7b1d13c831e11d4fe2a90
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-11-01 22:19:34 -0500

    xd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,14 @@
-const galeria = document.querySelector('.galeria');
-const contenedorImagenes = document.querySelector('.contenedor-imagenes');
-const botones = document.querySelectorAll('button');
-const anchoImagen = contenedorImagenes.clientWidth;
-let index = 0;
 
-botones.forEach((boton) => {
-    boton.addEventListener('click', () => {
-        if (boton.id === 'anterior') {
-            index = Math.max(index - 1, 0);
-        } else {
-            index = Math.min(index + 1, contenedorImagenes.children.length - 1);
-        }
-        const desplazamiento = -index * anchoImagen;
-        contenedorImagenes.style.transform = `translateX(${desplazamiento}px)`;
-    });
+
+const topbar = document.querySelector('.topbar');
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        topbar.style.position = 'fixed';
+        topbar.style.transform = 'translateY(-100px)'; 
+    } else {
+        topbar.style.position = 'relative';
+        topbar.style.transform = 'translateY(0)';
+    }
 });
